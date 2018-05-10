7e7772725c33e0c06aac2a8411953410e266a2f0
Anuradha Sharma <anuradhasharma084@gmail.com>
2018-05-10 15:46:26 -0400

    Basic add/delete/update

---
+++ RealmBasic/Model.swift
@@ -0,0 +1,23 @@
+//
+//  Model.swift
+//  RealmSwift
+//
+//  Created by Anuradha Sharma on 5/10/18.
+//  Copyright © 2018 Anuradha Sharma. All rights reserved.
+//
+
+import UIKit
+import RealmSwift
+
+class Model: Object {
+    
+    @objc dynamic var name : String?
+
+    @objc dynamic var id : String?
+    
+    
+    
+    override class func primaryKey() -> String? {
+        return "id"
+    }
+}
